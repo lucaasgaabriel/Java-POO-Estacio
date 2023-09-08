5aab0dbf5fc8a2c84a78395cd1592596014dd849
lucaasgaabriel <lucaas.gaabriel@live.com>
2023-09-08 20:04:10 -0300

    Adição de uma classe Main e puxando a classe Calculadora para construção de objeto

--- Calculadora.java
+++ Calculadora.java
@@ -17,16 +17,4 @@ public class Calculadora {
     public void div(){
         System.out.println("Divisão: " + (numero1 / numero2));
     }
-
-    public static void main (String[] args) {
-        Calculadora calc = new Calculadora();
-
-        calc.numero1 = 20;
-        calc.numero2 = 10;
-
-        calc.soma();
-        calc.sub();
-        calc.mult();
-        calc.div();
-    }
 }
